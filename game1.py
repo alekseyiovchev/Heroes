d19c828bc5bf8ab6c47988f69a6baedfa46fec05
Aleksey <tempoh0pe@yahoo.com>
2022-03-19 19:26:53 +0200

    updated

--- game1.py
+++ game1.py
@@ -1,57 +1,77 @@
 class Hero:
+    names = []
     population = 0
-    
     def __init__(self, name):
+        Hero.names.append(name)
+        Hero.heroes = " , ".join(Hero.names)
+
         self.name = name
         print(f'Зашел в игру {self.name}')
     
         Hero.population += 1
-        
-    def delete(self, ):
-        print(f'Покинул игру {self.name}')
+
+        Hero.sayHi(self.name)
+        Hero.HowMany()
+
+    def __delattr__(self, item):
+        print("--------------")
+        print(f'\nПокинул игру {self.name}')
         
         Hero.population -= 1
         
+        Hero.names.remove(self.name)
+        Hero.heroes = " , ".join(Hero.names)
+
         if Hero.population == 0:
             print('Это был последний герой.')
-            
+
+
         else:
             print(f'Количество героев {Hero.population}')
+      
+    def sayHi(name):
+        print(f'Вас приветствует {name}')
+          
+    @classmethod
+    def HowMany(cls):
+        print(f'В игре: {cls.population} \n')
+    
         
-    def sayHi(self, ):
-        print(f'Вас приветствует {self.name}')
-        
-    def HowMany():
-        print(f'В игре: {Hero.population} \n')
-        
-    HowMany = staticmethod(HowMany)
     
 hero1 = Hero('Zeus')
-hero1name = 'Zeus'
-hero1.sayHi()
-Hero.HowMany()
-
 hero2 = Hero('Lion')
-hero2name = 'Lion'
-hero2.sayHi()
-Hero.HowMany()
+hero3 = Hero('Snake')
 
 
-def kill(x = input('Исключить из игры - введите "Zeus" или "Lion":')):
-    if x == 'Zeus':
-            hero1.delete()
-            print(f'\n{hero2name} повезло и он остался в игре.')
+def kick():
+    while Hero.population:        
+        print(f"Герои: {Hero.heroes}")
+        x = input(f'Исключить из игры - введите название героя из списка:\n')
+        if x == hero1.name:
+            del hero1.name
+            print(f"\n{Hero.heroes} - повезло и они остались в игре.")
+            continue
 
-    elif x == 'Lion':
-            hero2.delete()
-            print(f"\n{hero1name} повезло и он остался в игре.")
+        elif x == hero2.name:
+            del hero2.name
+            print(f"\n{Hero.heroes} - повезло и они остались в игре.")
+            continue
+        
+        elif x == hero3.name:
+            del hero3.name
+            print(f"\n{Hero.heroes} - повезло и они остались в игре.")
+            continue
 
-    else:
+        else:
             print('Вы никого не исключили.')
+            break
 
 
+def main():
+        kick()
 
-kill()
-Hero.HowMany()
+if __name__ == "__main__":
+    main()
+
 
 
